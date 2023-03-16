aff348dfcf4c51be773a57244e9bed8221aed666
개발하는 김곰돌ᕕʕ •ₒ• ʔ୨ <rbgmll10331@gmail.com>
2023-03-16 15:36:45 +0900

    230316

--- MyPlayground#1.playground/Contents.swift
+++ MyPlayground#1.playground/Contents.swift
@@ -3,10 +3,21 @@ import UIKit
 var greeting = "Hello, playground"
 
 
-var a = 5
-var b = 8
+let a = ["a","d","s","s"]
+
+var q = a[Int.random(in: 0...3)]
+var ds = a[Int.random(in: 0...3)]
+
+print(q)
+print(ds)
+
+var p = "u"
+var o = "i"
+
+print(p)
+print(o)
+
+print(o+p)
 
 
-print(a)
-print(b)
-print(a, b)
+//print(pw)
