95dafa62d24bdbeb453923cf13e3d0f335c3ed42
개발하는 김곰돌ᕕʕ •ₒ• ʔ୨ <rbgmll10331@gmail.com>
2023-07-17 11:13:52 +0900

    Tipsy
    NSUnknownKeyException 해결

---
+++ note/BMI.swift
@@ -0,0 +1,15 @@
+//
+//  BMI.swift
+//  BMI Calculator
+//
+//  Created by 김규희 on 2023/07/14.
+//  Copyright © 2023 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+struct BMI {
+    let bmiValue : String
+    let advice : String
+    let color : UIColor
+}
